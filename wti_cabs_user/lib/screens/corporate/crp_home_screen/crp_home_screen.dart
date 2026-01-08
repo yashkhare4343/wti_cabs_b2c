a02ac6599b33effbc5cd741cb9ebb17533a959e2
ASND Technologies <asndtechnologies@ASNDs-MacBook-Air.local>
2026-01-08 14:50:07 +0530

    crp app-demo-8 jan completed

--- wti_cabs_user/lib/screens/corporate/crp_home_screen/crp_home_screen.dart
+++ wti_cabs_user/lib/screens/corporate/crp_home_screen/crp_home_screen.dart
@@ -1288,49 +1288,49 @@ class _TopBannerState extends State<TopBanner> {
                         SizedBox(
                           width: 12,
                         ),
-                        // Container(
-                        //   height: 35,
-                        //   decoration: BoxDecoration(
-                        //     /*gradient: const LinearGradient(
-                        //                   colors: [Color(0xFF0052D4), Color(0xFF4364F7), Color(0xFF6FB1FC)],
-                        //                   begin: Alignment.centerLeft,
-                        //                   end: Alignment.centerRight,
-                        //                 ),*/
-                        //     color: AppColors.mainButtonBg,
-                        //     borderRadius: BorderRadius.circular(24),
-                        //   ),
-                        //   child: ElevatedButton(
-                        //     style: ElevatedButton.styleFrom(
-                        //       backgroundColor:
-                        //           Colors.transparent, // transparent to show gradient
-                        //       shadowColor:
-                        //           Colors.transparent, // remove default shadow
-                        //       shape: RoundedRectangleBorder(
-                        //         borderRadius: BorderRadius.circular(16),
-                        //       ),
-                        //       padding: const EdgeInsets.symmetric(
-                        //           vertical: 4, horizontal: 8),
-                        //     ),
-                        //     onPressed: () async {
-                        //       await _goToPersonal(context);
-                        //     },
-                        //     child: const Text(
-                        //       "Retail",
-                        //       style: TextStyle(
-                        //         color: Colors.white,
-                        //         fontSize: 12,
-                        //         fontWeight: FontWeight.w600,
-                        //         fontFamily: 'Montserrat',
-                        //       ),
-                        //     ),
-                        //   ),
-                        // )
-                        InkWell(
-                          onTap: ()async{
-                            await _goToPersonal(context);
-                          },
-                            child: Image.asset('assets/images/goRetail.png', width: 120, height: 36,   fit: BoxFit.contain,
-                            ),)
+                        Container(
+                          height: 35,
+                          decoration: BoxDecoration(
+                            /*gradient: const LinearGradient(
+                                          colors: [Color(0xFF0052D4), Color(0xFF4364F7), Color(0xFF6FB1FC)],
+                                          begin: Alignment.centerLeft,
+                                          end: Alignment.centerRight,
+                                        ),*/
+                            color: AppColors.mainButtonBg,
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Colors.transparent, // transparent to show gradient
+                              shadowColor:
+                                  Colors.transparent, // remove default shadow
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 4, horizontal: 8),
+                            ),
+                            onPressed: () async {
+                              await _goToPersonal(context);
+                            },
+                            child: const Text(
+                              "Retail",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                                fontFamily: 'Montserrat',
+                              ),
+                            ),
+                          ),
+                        )
+                        // InkWell(
+                        //   onTap: ()async{
+                        //     await _goToPersonal(context);
+                        //   },
+                        //     child: Image.asset('assets/images/goRetail.png', width: 120, height: 36,   fit: BoxFit.contain,
+                        //     ),)
                         // upcomingBookingController
                         //             .isLoggedIn.value ==
                         //         true
